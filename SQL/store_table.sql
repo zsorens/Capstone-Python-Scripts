a00a4ec90c3bbad7a3714f334bebc4db3b2b2c7e
AdamTNoble <157399974+AdamTNoble@users.noreply.github.com>
2024-04-19 09:28:31 -0400

    More SQL

--- SQL/store_table.sql
+++ SQL/store_table.sql
@@ -5,9 +5,9 @@ DROP TABLE IF EXISTS [stores];
 CREATE TABLE [stores](
 	place_id varchar(50) primary key,
 	flag_text bit NOT NULL default 0,
-	flag_image bit NOT NULL default 0,
+	flag_image bit NOT NULL default 0, --remove this from UI 
 	flag_website bit NOT NULL default 0,
-	last_updated datetime NOT NULL default GETDATE(), -- used when we suesd to have meta data but thats illegl
+	last_updated datetime NOT NULL default GETDATE(), -- used when we suesd to have meta data but thats illegl 
 	last_flagged datetime,
 	flag_street bit NOT NULL default 0,
 	flag_review_image bit NOT NULL default 0,
